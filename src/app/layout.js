7ef8674efe7de3a5181466068204a67394a522c3
khnbzkrt <khan.bozkurt@gmail.com>
2023-06-11 20:41:20 +0300

    metadata removed

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,16 +7,6 @@ import '@/styles/reset.css';
 import '@/styles/fonts.css';
 import '@/styles/globals.css';
 
-export const metadata = {
-  title: 'Find Developer',
-  description: 'Find your developer',
-  viewport: {
-    width: 'device-width',
-    initialScale: 1,
-    maximumScale: 1,
-  },
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
