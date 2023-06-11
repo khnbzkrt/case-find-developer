f3cf8ce4bf1d2e70ed28ee2e6f10f250581c2af3
khnbzkrt <khan.bozkurt@gmail.com>
2023-06-11 20:48:35 +0300

    fix

--- src/components/index.js
+++ src/components/index.js
@@ -1,41 +1,41 @@
 // Atoms
-export { default as FilterLinkButton } from './atoms/buttons/filter-link-button/FilterLinkButton';
-export { default as PrimaryLinkButton } from './atoms/buttons/primary-link-button/PrimaryLinkButton';
-export { default as SearchButton } from './atoms/buttons/search-button/SearchButton';
 export { default as Dropdown } from './atoms/dropdown/Dropdown';
-export { default as SocialMediaLogo } from './atoms/logo/SocialMediaLogo';
-export { default as SectionTitle } from './atoms/titles/section-title/SectionTitle';
+export { default as FilterLinkButton } from './atoms/buttons/filter-link-button/FilterLinkButton';
 export { default as IconTitle } from './atoms/titles/icon-title/IconTitle';
 export { default as IconTitleSmall } from './atoms/titles/icon-title/IconTitleSmall';
+export { default as PrimaryLinkButton } from './atoms/buttons/primary-link-button/PrimaryLinkButton';
+export { default as SearchButton } from './atoms/buttons/search-button/SearchButton';
+export { default as SectionTitle } from './atoms/titles/section-title/SectionTitle';
+export { default as SocialMediaLogo } from './atoms/logo/SocialMediaLogo';
 
 // Molecules
+export { default as HorizontalLink } from './molecules/horizontal-link/HorizontalLink';
+export { default as IconTextLabel } from './molecules/icon-text-label/IconTextLabel';
 export { default as IntroSlogan } from './molecules/intro-slogan/IntroSlogan';
 export { default as MainSlider } from './molecules/main-slider/MainSlider';
 export { default as Navbar } from './molecules/navbar/Navbar';
-export { default as SocialMediaNavbar } from './molecules/navbar/SocialMediaNavbar';
 export { default as NavbarProfile } from './molecules/navbar-profile/NavbarProfile';
-export { default as SearchInput } from './molecules/search-input/SearchInput';
-export { default as SuggestionList } from './molecules/suggestion-list/SuggestionList';
-export { default as HorizontalLink } from './molecules/horizontal-link/HorizontalLink';
-export { default as IconTextLabel } from './molecules/icon-text-label/IconTextLabel';
 export { default as ProfileImage } from './molecules/profile-image/ProfileImage';
+export { default as SearchInput } from './molecules/search-input/SearchInput';
+export { default as SocialMediaNavbar } from './molecules/navbar/SocialMediaNavbar';
+export { default as SuggestionList } from './molecules/suggestion-list/SuggestionList';
 
 // Organisms
-export { default as CompanySection } from './organisms/company-section/CompanySection';
 export { default as Certificate } from './organisms/certificate/Certificate';
+export { default as CompanySection } from './organisms/company-section/CompanySection';
 export { default as Education } from './organisms/education/Education';
 export { default as Experience } from './organisms/experience/Experience';
 export { default as Footer } from './organisms/footer/Footer';
 export { default as Header } from './organisms/header/Header';
-export { default as SocialMediaHeader } from './organisms/header/SocialMediaHeader';
 export { default as Intro } from './organisms/Intro/Intro';
 export { default as JobPost } from './organisms/job-post/JobPost';
 export { default as ObjectList } from './organisms/object-list/ObjectList';
 export { default as PopularSearch } from './organisms/popular-search/PopularSearch';
-export { default as ServiceSection } from './organisms/service-section/ServiceSection';
 export { default as ProfileInfoSection } from './organisms/profile-info-section/ProfileInfoSection';
 export { default as ProfileSidebar } from './organisms/profile-sidebar/ProfileSidebar';
 export { default as SearchBar } from './organisms/search-bar/SearchBar';
+export { default as ServiceSection } from './organisms/service-section/ServiceSection';
+export { default as SocialMediaHeader } from './organisms/header/SocialMediaHeader';
 export { default as SuggestionSection } from './organisms/suggestion-section/SuggestionSection';
 export { default as UserInformation } from './organisms/user-information/UserInformation';
 export { default as WebSite } from './organisms/web-site/WebSite';
